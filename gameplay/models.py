c4d0cabf1aff5a0fade39ee279fc88c1b7da70f2
Manuel Soberanis <msoberanis@grupoplenum.local>
2019-02-01 10:09:40 -0600

    initial commit

---
+++ gameplay/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.encoding import python_2_unicode_compatible
+from django.db.models import Q
+# Create your models here.
+
+
+GAME_STATUS_CHOICES=(
+    ('F','First Player To Move'),
+    ('S', 'Second Player To move'),
+    ('W','First Player Wins'),
+    ('L','Second Player Wins'),
+    ('D', 'Draw')
+)
+
+class GamesQuerySet(models.QuerySet):
+    def games_for_user(self, user):
+        return self.filter(
+            Q(first_player=user) | Q(second_player=user)
+        )
+
+    def active(self):
+        return self.filter(
+            Q(status='F') | Q(status='S')
+        )
+
+
+@python_2_unicode_compatible
+class Game(models.Model):
+    first_player = models.ForeignKey(User, related_name="games_first_player", on_delete=models.PROTECT)
+    second_player = models.ForeignKey(User, related_name="games_second_player", on_delete=models.PROTECT)
+    start_time = models.DateTimeField(auto_now_add=True)
+    last_active = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=1, default='F', choices=GAME_STATUS_CHOICES)
+    def __str__(self):
+        return "{0} vs {1} - {2}".format(self.first_player, self.second_player, self.status)
+    objects = GamesQuerySet.as_manager()
+
+
+class Move(models.Model):
+    x = models.IntegerField()
+    y = models.IntegerField()
+    comment = models.CharField(max_length=300, blank=True)
+    by_first_player = models.BooleanField()
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
